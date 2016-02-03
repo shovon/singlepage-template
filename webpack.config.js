c6c3ac35e95711b53ac52b8e85a54992d91f272f
Salehen Shovon Rahman <salehen.rahman@gmail.com>
2016-02-02 17:07:12 -0800

    Initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/entry.ts',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel?'+JSON.stringify({
+          presets: [ 'es2015' ]
+        })
+      },
+      { test: /\.ts$/, loader: 'ts-loader' }
+    ]
+  }
+};
